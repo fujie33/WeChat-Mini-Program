5607a64844d5a1089abc2ad840a2a08393e1d209
fujie33 <2990989544@qq.com>
2021-01-26 21:34:06 +0800

    Initial Commit

---
+++ miniprogram/pages/music/music.js
@@ -0,0 +1,123 @@
+// pages/music/music.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [{
+        url: 'http://p1.music.126.net/9Ayx-EeCnuLRWKTcIhGB6g==/109951165664742856.jpg?imageView&quality=89'
+      },
+      {
+        url: 'http://p1.music.126.net/j0gp3gBDRRoqIXxAs0v7oA==/109951165664720877.jpg?imageView&quality=89'
+      },
+      {
+        url: 'http://p1.music.126.net/q5rKcBx9Y0V37DsUSaQKXg==/109951165664695730.jpg?imageView&quality=89'
+      },
+      {
+        url: 'http://p1.music.126.net/Z90NF2dHuBYrV6x-U9jJJQ==/109951165664719544.jpg?imageView&quality=89'
+      },
+      {
+        url: 'http://p1.music.126.net/UdSM2BmqY_h_t9HAOzb5dQ==/109951165664710664.jpg?imageView&quality=89'
+      },
+      {
+        url: 'http://p1.music.126.net/C9I9GxpvRX7nCZyXNBeqOw==/109951165664694558.jpg?imageView&quality=89'
+      },
+      {
+        url: 'http://p1.music.126.net/WOoIZuva_umxxzYOvWINLA==/109951165664707565.jpg?imageView&quality=89'
+      },
+      {
+        url: 'http://p1.music.126.net/nVUH7O5ZNMG1OQ1kE-tq9g==/109951165665595417.jpg?imageView&quality=89'
+      }
+    ],
+    playlist: [
+      {"id": "1001",
+      "playCount": 1.4641238e+06,
+      "name": "韩甜 | 轻快旋律·山风不及你温柔",
+      "picUrl": "http://p4.music.126.net/UpyW_JOHIP2mNCGSYtyUhA==/109951165131295355.jpg?param=140y140"
+      },
+      {"id": "1002",
+      "playCount": 1.6661238e+06,
+      "name": "做我的公主吧 我把皇冠给你",
+      "picUrl": "http://p4.music.126.net/ZJJ1ZKZ9_myIdOMKqT2LAQ==/109951165426759607.jpg?param=140y140"
+      },
+      {"id": "1003",
+      "playCount": 1.2761238e+06,
+      "name": "[宝藏系女孩] 值得你红心收藏的宝藏女声都在这里",
+      "picUrl": "http://p4.music.126.net/Nap93CAJm8uJHVG39wWb3g==/109951165545380835.jpg?param=140y140"
+      },
+      {"id": "1004",
+      "playCount": 1.9641238e+06,
+      "name": "欧美 | 前奏跪 × 开口脆",
+      "picUrl": "http://p4.music.126.net/t5lalcY7dQq2dhcXB3EtVA==/109951163434572045.jpg?param=140y140"
+      },
+      {"id": "1005",
+      "playCount": 622822.5,
+      "name": "人生理想 和你甜一辈子",
+      "picUrl": "http://p3.music.126.net/Hg2V2SZFAoSDRMpef5oTsQ==/109951165539572801.jpg?param=140y140"
+      },
+      {"id": "1006",
+      "playCount": 1.3841238e+06,
+      "name": "耳机里的温柔乡，这些歌可以引来超甜的恋爱",
+      "picUrl": "http://p4.music.126.net/ncZr0b28Ps8hFpvqd5QpXQ==/109951165074517623.jpg?param=140y140"
+      }
+    ]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
